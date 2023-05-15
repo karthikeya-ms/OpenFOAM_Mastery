9a96086e5bbf9314ed2ca3560d1d850f364f3af8
Siva Karthikeya <writetokarthikeya99@gmail.com>
2023-05-15 12:46:08 +0200

    first commit

---
+++ basicCode/hello_world.cpp
@@ -0,0 +1,10 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!"<<std::endl;
+    std::cout << "I am an OpenFOAM fan" <<std::endl;
+    return 0;
+}
+
